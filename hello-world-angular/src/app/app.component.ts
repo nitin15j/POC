cfb1189b8cb7b507325c24b5e286656ebf390a8e
n.x.jain <n.x.jain@accenture.com>
2018-05-16 08:46:40 +0530

    development branch changes

--- hello-world-angular/src/app/app.component.ts
+++ hello-world-angular/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app-hello-world';
+  title = 'app-hello-world-development';
 }
